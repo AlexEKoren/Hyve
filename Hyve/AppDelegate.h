c4f7effedd75f55fbdb6a044c999070549b92fb0
Alex Koren <alexkoren@MAC04011-CUE-Laptop.local>
2013-08-12 11:33:59 -0400

    Initial Commit

---
+++ Hyve/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hyve
+//
+//  Created by Alex Koren on 8/12/13.
+//  Copyright (c) 2013 AlexAndSheldonApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hyve/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hyve
+//
+//  Created by Alex Koren on 8/12/13.
+//  Copyright (c) 2013 AlexAndSheldonApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
